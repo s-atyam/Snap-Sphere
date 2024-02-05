c082d8e51222f9b8c22af363a85c6a828cb823dc
Satyam <satyambharti080@gmail.com>
2024-02-06 00:52:27 +0530

    few fixes in queries

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -59,14 +59,15 @@ import {
     return useInfiniteQuery({
       queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
       queryFn: getInfinitePosts,
-      getNextPageParam: (lastPage) => {
+      initialPageParam: undefined,
+      getNextPageParam: (lastPage: any) => {
         // If there's no data, there are no more pages.
         if (lastPage && lastPage.documents.length === 0) return null
         
         // Use the $id of the last document as the cursor.
         const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
         return lastId;
-      },
+      }
     });
   };
   
